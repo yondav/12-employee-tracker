85c17c361cade48171072a68d92fdcb1eb69ddce
yondav <yonidavid.abr@gmail.com>
2021-04-18 02:52:00 -0400

    initial commit, schema and seed files set up and tested.

---
+++ db/schema.sql
@@ -0,0 +1,28 @@
+DROP DATABASE IF EXISTS employee_trackerdb;
+CREATE database employee_trackerdb;
+
+USE employee_trackerdb;
+
+CREATE TABLE department (
+  id INT NOT NULL AUTO_INCREMENT,
+  name VARCHAR(30) NOT NULL, -- department name
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE role (
+  id INT NOT NULL AUTO_INCREMENT,
+  title VARCHAR(30), -- role title
+  salary DECIMAL(11,2),
+  department_id INT, -- ref to department
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE employee (
+  id INT NOT NULL AUTO_INCREMENT,
+  first_name VARCHAR(30),
+  last_name VARCHAR(30),
+  role_id INT, -- ref to employee role
+  manager_id INT, -- ref to another employee (manager)
+
+  PRIMARY KEY (id)
+);
---
+++ db/seed.sql
@@ -0,0 +1,46 @@
+-- department table
+INSERT INTO department(name)
+VALUES
+("Writing"), 
+("Performance"), 
+("Executive"), 
+("Production");
+
+-- role table
+INSERT INTO role(title, salary, department_id)
+VALUES
+-- writing
+("Head Writer", 500000.00, 1),
+("Writer", 200000.00, 1),
+("Assistant", 65000.00, 1),
+-- performance
+("Actor", 1000000.00, 2),
+-- executive
+("Vice President of East Coast Television and Microwave Oven Programming", 9000000.00, 3),
+("CEO", 9999999.99, 3),
+("Vice President of West Coast News, Web Content, and Theme Park Talent Relations", 3000000.00, 3),
+("Assistant", 100000.00, 3),
+("Page", 50000.00, 3),
+-- production
+("Producer", 500000.00, 4); 
+
+-- employee table
+INSERT INTO employee(first_name, last_name, role_id, manager_id)
+VALUES
+-- writing
+("Liz", "Lemon", 1, 8),
+("Frank", "Rossitano", 2, 1),
+("Toofer", "Spurlock", 2, 1),
+("Johnny", "Lutz", 2, 1),
+("Cerie", "Xerox", 3, 1),
+-- performance
+("Jenna", "Maroney", 4, 13),
+("Tracey", "Jordan", 4, 13),
+-- executive
+("Jack", "Donaghy", 5, 9),
+("Don", "Geiss", 6, NULL),
+("Devon", "Banks", 7, 9),
+("Jonathan", NULL, 8, 8),
+("Kenneth", "Parcell", 9, 8),
+-- production
+("Pete", "Hornberger", 10, 8);
