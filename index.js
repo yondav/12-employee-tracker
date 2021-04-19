d485d1a97fa9eca523d225414d31cacf35601044
yondav <yonidavid.abr@gmail.com>
2021-04-18 20:50:36 -0400

    haven't messed with any of the logic yet. Was experimenting with figlet and chalk. Configured for use in the project

--- index.js
+++ index.js
@@ -9,10 +9,15 @@
 const mysql = require('mysql');
 const inquirer = require('inquirer');
 const console_table = require('console.table');
+const figlet = require('figlet');
+const chalk = require('chalk');
+
+const title = require('./lib/title');
 
 // mysql password
 // change file path to './config' once you've added your mysql password to the config file. See README
 const pass = require('./pass');
+const { launchTitle, appTitle, depTitle, roleTitle, employeeTitle } = require('./lib/title');
 
 // connection to db
 const connection = mysql.createConnection({
@@ -22,3 +27,9 @@ const connection = mysql.createConnection({
   password: pass,
   database: 'employee_trackerdb',
 });
+
+console.log(title);
+launchTitle(appTitle.hex, appTitle.text);
+launchTitle(depTitle.hex, depTitle.text);
+launchTitle(roleTitle.hex, roleTitle.text);
+launchTitle(employeeTitle.hex, employeeTitle.text);
