dded0930a3882b060de31344e936a7994b1a5b98
yondav <yonidavid.abr@gmail.com>
2021-04-22 02:54:10 -0400

    cleaning up

--- index.js
+++ index.js
@@ -12,30 +12,30 @@ const console_table = require('console.table');
 // modules
 const prompts = require('./lib/prompts');
 
-const init = async () => {
+const init = () => {
   // prompts.optionsQuery(`SELECT name FROM department`);
   prompts.welcomePrompt();
   inquirer.prompt(prompts.enter).then((res) => {
-    if (res.continue === true) {
-      inquirer.prompt(prompts.menuPrompt).then((res) => {
-        switch (res.menu) {
-          case 'view':
-            prompts.viewTable();
-            break;
-          case 'add':
-            prompts.add();
-            break;
-          case 'edit':
-            prompts.edit();
-            break;
-          case 'remove':
-            prompts.remove();
-            break;
-        }
-      });
-    } else {
-      init();
-    }
+    res.continue === true
+      ? inquirer.prompt(prompts.menuPrompt).then((res) => {
+          switch (res.menu) {
+            case 'view':
+              prompts.viewTable().then(() => {
+                inquirer.prompt(prompts.enter).then((res) => (res.continue === true ? init.init() : process.exit(0)));
+              });
+              break;
+            case 'add':
+              prompts.add();
+              break;
+            case 'edit':
+              prompts.edit();
+              break;
+            case 'remove':
+              prompts.remove();
+              break;
+          }
+        })
+      : init();
   });
 };
 
