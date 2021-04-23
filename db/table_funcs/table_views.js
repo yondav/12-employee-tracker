e1bf72a66da43215961b69c273268156f4a540c0
yondav <yonidavid.abr@gmail.com>
2021-04-23 02:16:08 -0400

    continuing refactor of callback hell

--- db/table_funcs/table_views.js
+++ db/table_funcs/table_views.js
@@ -12,46 +12,46 @@ const chalk = require('chalk');
 
 // display all employees
 const empTable = (cb) => {
-  let query = `SELECT employee.id, employee.last_name, employee.first_name, role.title, department.name 
-  AS department, CONCAT('$',role.salary)
-  AS salary, CONCAT(manager.last_name, ', ', manager.first_name) 
-  AS manager FROM employee 
-  LEFT JOIN role 
-  On employee.role_id = role.id 
-  LEFT JOIN department 
-  ON role.department_id = department.id 
-  LEFT JOIN employee manager 
-  ON manager.id = employee.manager_id`;
+  let query = `SELECT employee.id, employee.last_name, employee.first_name, role.title, department.name AS department, CONCAT('$',role.salary) AS salary, CONCAT(manager.last_name, ', ', manager.first_name) AS manager 
+  FROM employee 
+  LEFT JOIN role ON employee.role_id = role.id 
+  LEFT JOIN department ON role.department_id = department.id 
+  LEFT JOIN employee manager ON manager.id = employee.manager_id`;
+
+  let totalSal = `SELECT CONCAT('$', SUM(role.salary) OVER()) AS total_salary 
+  FROM employee 
+  LEFT JOIN role ON employee.role_id = role.id 
+  LEFT JOIN department ON role.department_id = department.id 
+  LEFT JOIN employee manager ON manager.id = employee.manager_id
+  LIMIT 1`;
 
   connection.query(query, (err, res) => {
     if (err) throw err;
     console.table(res);
+  });
+
+  connection.query(totalSal, (err, res) => {
+    if (err) throw err;
+    console.log(chalk.hex('#E47474').bgHex('#000000')('\n | Total Payroll: ' + res[0].total_salary + ' | \n'));
     cb();
   });
 };
 
 // display employees by department or role
 const viewByTable = (key, val, cb) => {
-  let query = `SELECT employee.id, employee.last_name, employee.first_name, role.title, department.name 
-  AS department, CONCAT('$',role.salary)
-  AS salary, CONCAT(manager.last_name, ', ', manager.first_name) 
-  AS manager FROM employee
-  LEFT JOIN role 
-  On employee.role_id = role.id 
-  LEFT JOIN department 
-  ON role.department_id = department.id 
+  let query = `SELECT employee.id, employee.last_name, employee.first_name, role.title, department.name AS department, CONCAT('$',role.salary) AS salary, CONCAT(manager.last_name, ', ', manager.first_name) AS manager 
+  FROM employee
+  LEFT JOIN role ON employee.role_id = role.id 
+  LEFT JOIN department ON role.department_id = department.id 
   LEFT JOIN employee manager 
   ON manager.id = employee.manager_id
   WHERE ${key} = ?`;
 
   let totalSal = `SELECT CONCAT('$', SUM(role.salary) OVER())
   AS total_salary FROM employee
-  LEFT JOIN role 
-  On employee.role_id = role.id 
-  LEFT JOIN department 
-  ON role.department_id = department.id 
-  LEFT JOIN employee manager 
-  ON manager.id = employee.manager_id
+  LEFT JOIN role ON employee.role_id = role.id 
+  LEFT JOIN department ON role.department_id = department.id 
+  LEFT JOIN employee manager ON manager.id = employee.manager_id
   WHERE ${key} = ?
   LIMIT 1;`;
 
