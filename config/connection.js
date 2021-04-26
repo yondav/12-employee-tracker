a06ece8733fab2f70db8afd2467f5e6e35c7b0ff
yondav <yonidavid.abr@gmail.com>
2021-04-25 20:23:20 -0400

    working on setup for sequelize version

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,7 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
 
-console.log(process.env);
+// console.log(process.env);
 const sequelize = new Sequelize(
   process.env.DB_NAME,
   process.env.DB_USER,
